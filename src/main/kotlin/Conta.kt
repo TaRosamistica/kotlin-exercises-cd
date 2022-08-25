9b10b4aecf7ae333ac7d96af65d1537002ace84b
Talita Oliveira <talita.rmsoareso@gmail.com>
2022-08-25 00:45:12 -0300

    adiciona método de saque e transferência

--- src/main/kotlin/Conta.kt
+++ src/main/kotlin/Conta.kt
@@ -1,12 +1,39 @@
-class Conta (
-    val titular: String,
-    val numero: Int,
-    var saldo: Double)
-{
+class Conta (val titular: String, val numero: Int, saldo: Double){
+    private var saldo: Double = if (saldo > 0) saldo else 0.0
+
+fun getSaldo(): Double {
+    return this.saldo
+}
+
 fun deposita (valor: Double) {
-    if(valor in 0.0..10.000) {
+    if(valor in 0.0..10000.0) {
         this.saldo += valor
-    } else throw IllegalArgumentException ("O valor de depósito deve ser entre 1 e 10.000")
+    } else throw IllegalArgumentException ("O valor de depósito deve ser entre 1 e 10000.0")
 
 }
+fun saca (valor: Double) {
+    if (saldo >= valor) {
+        saldo -= valor
+    } else {
+        throw IllegalArgumentException("Saldo insuficiente")
+    }
 }
+
+fun transfere(valor: Double, destino: Conta): Double{
+    try {
+        this.saca(valor)
+        destino.deposita(valor)
+        return this.saldo
+    } catch (e: IllegalArgumentException) {
+        return this.saldo
+    }
+
+}
+
+}
+
+
+
+
+
+
--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,10 +1,19 @@
 fun main (){
 
-val contaTalita: Conta = Conta("Talita", 1000, 1000.00)
+val contaTalita: Conta = Conta("Talita", 1000, 1000.0)
     println(contaTalita.titular)
+    println(contaTalita.getSaldo())
 
-    contaTalita.deposita(10.000)
-    println(contaTalita.saldo)
+    contaTalita.deposita(500.0)
+    println(contaTalita.getSaldo())
+    contaTalita.saca(2.00)
+    //println(contaTalita.getSaldo())
 
+val contaMonica: Conta = Conta("Monica", 2000, 100.0)
+    contaMonica.saca(50.0)
+    println(contaMonica.getSaldo())
+
+    println(contaMonica.transfere(11000.0, contaTalita))
+    println(contaTalita.getSaldo())
 
 }
