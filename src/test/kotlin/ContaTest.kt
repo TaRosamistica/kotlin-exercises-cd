cb6ca3b5730636e173a3460062952b295dc2a4b2
Talita Oliveira <talita.rmsoareso@gmail.com>
2022-08-29 15:24:24 -0300

    refatora testes unitários organizando por cenário

--- src/test/kotlin/ContaTest.kt
+++ src/test/kotlin/ContaTest.kt
@@ -1,67 +1,103 @@
-import org.junit.jupiter.api.Assertions.assertEquals
-import org.junit.jupiter.api.Assertions.assertTrue
-import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertThrows
+import org.junit.jupiter.api.Assertions.assertTrue
+import org.junit.jupiter.api.Assertions.assertEquals
 
-internal class ContaTest {
+class ContaTest {
 
-    private lateinit var contaTalita: Conta
-    private lateinit var contaMonica: Conta
-    private lateinit var contaNegativa: Conta
-
-    // TODO - Verificar organizacao de testes por cenario
-
-    @BeforeEach
-    fun setUp() {
-        contaTalita = Conta("Talita", 0, 10.0)
-        contaMonica = Conta("Monica", 0, 20000.0)
-        contaNegativa = Conta("Maria", 0, -100.0)
+    @Test
+    fun `Quando criar uma conta, o valor do saldo não pode ser negativo`() {
+        val contaMaria = Conta("Maria", 1000, -100.0)
+        assertEquals(contaMaria.getSaldo(), 0.0)
     }
 
     @Test
-    fun getSaldo() {
-        assertEquals(contaTalita.getSaldo(), 10.0)
-        assertEquals(contaMonica.getSaldo(), 20000.0)
-        assertEquals(contaNegativa.getSaldo(), 0.0)
-
+    fun `Quando criar uma conta, o valor do saldo deve ser maior ou igual a zero`() {
+        val contaMaria = Conta("Maria", 1000, 100.0)
+        assertEquals(contaMaria.getSaldo(), 100.0)
     }
 
+    // TODO - Cenario feliz de saldo
+
     @Test
-    fun deposita() {
+    fun `Quando o valor depositado for maior que dez mil (10000), deve lançar uma exceção`() {
+        val contaMonica = Conta("Monica", 2000, 20000.0)
         assertThrows<IllegalArgumentException> { contaMonica.deposita(11000.0) }
+    }
+
+    @Test
+    fun `Quando o valor depositado for menor que zero, deve lançar exceção`() {
+        val contaMonica = Conta("Monica", 2000, 20000.0)
         assertThrows<IllegalArgumentException> { contaMonica.deposita(-10.0) }
-        assertTrue(contaMonica.deposita(10.0))
     }
 
     @Test
-    fun saca() {
-        assertEquals(contaMonica.getSaldo(), 20000.0)
+    fun `Quando valor depositado for entre zero e dez mil, o valor deve ser adicionado ao saldo da conta`() {
+        val contaTalita = Conta("Talita", 3000, 10.0)
+        assertTrue(contaTalita.deposita(200.0))
+        assertEquals(contaTalita.getSaldo(), 210.0)
+    }
+
+    @Test
+    fun `Quando o valor de saque for maior que o saldo da conta, deve lançar uma exceção`() {
+        val contaMaria = Conta("Maria", 1000, -100.0)
+        assertThrows<IllegalArgumentException> { contaMaria.saca(20.0) }
+    }
+
+    @Test
+    fun `Quando valor de saque for menor que zero, deve lançar uma exceção`() {
+        val contaMonica = Conta("Monica", 2000, 20000.0)
         assertThrows<IllegalArgumentException> { contaMonica.saca(-1.0) }
-        assertEquals(contaMonica.getSaldo(), 20000.0)
-
-        assertThrows<IllegalArgumentException> { contaNegativa.saca(10.0) }
-        assertEquals(contaNegativa.getSaldo(), 0.0)
-
-        assertTrue(contaTalita.saca(10.0))
-        assertEquals(contaTalita.getSaldo(), 0.0)
     }
 
     @Test
-    fun transfere() {
-        assertEquals(contaTalita.getSaldo(), 10.0)
-        assertEquals(contaMonica.getSaldo(), 20000.0)
+    fun `Quando valor de saque for igual que o valor do saldo, deve executar o saque`() {
+        val contaTalita = Conta("Talita", 3000, 10.0)
+        assertTrue(contaTalita.saca(10.0))
+        assertEquals(contaTalita.getSaldo(),0.0)
+    }
+
+    @Test
+    fun `Quando valor de saque for menor que o valor do saldo, deve executar o saque`() {
+        val contaTalita = Conta("Talita", 3000, 10.0)
+        assertTrue(contaTalita.saca(5.0))
+        assertEquals(contaTalita.getSaldo(), 5.0)
+    }
+
+    @Test
+    fun `Quando valor da transferência for maior que o valor do saldo, deve lançar uma exceção`() {
+        val contaMaria = Conta("Maria", 1000, 10.0)
+        val contaTalita = Conta("Talita", 3000, 20.0)
+        assertThrows<IllegalArgumentException> { contaMaria.transfere(100.0, contaTalita) }
+    }
+
+    @Test
+    fun `Quando valor da transferência for maior que dez mil, deve lançar uma exceção`() {
+        val contaMonica = Conta("Monica", 2000, 20000.0)
+        val contaTalita = Conta("Talita", 3000, 10.0)
+        assertThrows<IllegalArgumentException> { contaMonica.transfere(11000.0, contaTalita) }
+    }
+
+    @Test
+    fun `Quando valor da transferência for negativo, deve lançar uma exceção`() {
+        val contaTalita = Conta("Talita", 3000, 10.0)
+        val contaMaria = Conta("Maria", 1000, -100.0)
+        assertThrows<IllegalArgumentException> { contaTalita.transfere(-200.0, contaMaria) }
+    }
+
+    @Test
+    fun `Quando valor da transferência for igual ao valor do saldo, deve realizar a transferência`() {
+        val contaTalita = Conta("Talita", 3000, 10.0)
+        val contaMonica = Conta("Monica", 2000, 20000.0)
         assertTrue(contaTalita.transfere(10.0, contaMonica))
         assertEquals(contaTalita.getSaldo(), 0.0)
-        assertEquals(contaMonica.getSaldo(), 20010.0)
 
-        assertTrue(contaMonica.transfere(10000.0, contaTalita))
-        assertEquals(contaMonica.getSaldo(), 10010.0)
-        assertEquals(contaTalita.getSaldo(), 10000.0)
-
-        assertThrows<IllegalArgumentException> { contaMonica.transfere(10010.0, contaTalita) }
-        assertThrows<IllegalArgumentException> { contaNegativa.transfere(100.0, contaTalita) }
-        assertThrows<IllegalArgumentException> { contaNegativa.transfere(-100.0, contaTalita) }
     }
-
+    @Test
+    fun `Quando valor da transferência for menor que o valor do saldo, deve deve realizar a transferência`() {
+        val contaTalita = Conta("Talita", 3000, 500.0)
+        val contaMonica = Conta("Monica", 2000, 20000.0)
+        assertTrue(contaTalita.transfere(100.0, contaMonica))
+        assertEquals(contaTalita.getSaldo(), 400.0)
+    }
 }
